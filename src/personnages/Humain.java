d5962fa971c7088c89109a4dfb05ac51e8feabe5
Sayagh Touria <94766960+touriasay@users.noreply.github.com>
2022-11-10 15:34:10 +0100

    test

---
+++ src/personnages/Humain.java
@@ -0,0 +1,46 @@
+package personnages;
+
+public class Humain {
+ 
+	private String nom;
+	private int argent;
+	private String boisson;
+	
+	
+	public Humain(String nom, int argent, String boisson) {
+	
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("("+this.nom+") - "+texte);
+	}
+	
+	public void direbonjour() {
+		System.out.println("Bonjour ! Je m’appelle "+this.nom+
+							" et j’aime boire du"+this.boisson+"et j'ai"+this.argent+"sous en poche");
+	}
+	
+	public void boire() {
+	parler("Mmmm, un bon verre de" +this.boisson+ " ! GLOUPS !");
+		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public String getBoisson() {
+		return boisson;
+	}
+	
+	
+
+	
+}
